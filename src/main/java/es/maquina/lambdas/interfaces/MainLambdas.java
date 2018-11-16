84d5aa0158090c451a293be4ab867c7a7d325121
Christian Muñoz Asón <cmunozas@everis.com>
2018-11-17 00:30:58 +0100

    Creacion de ejemplo de lambda con 1 hilo y javadoc

--- src/main/java/es/maquina/lambdas/interfaces/MainLambdas.java
+++ src/main/java/es/maquina/lambdas/interfaces/MainLambdas.java
@@ -16,6 +16,11 @@ public class MainLambdas {
     private static final Logger LOGGER = Logger.getLogger(MainLambdas.class.getName());
 
     /**
+     * Constante de tipo int que indica el tiempo de espera del hilo
+     */
+    private static final int TIEMPO_ESPERA_HILO = 2000;
+
+    /**
      * Metodo usado para la ejecucion del programa puedes comentar los metodos para
      * ejecutar los que te interesen
      * 
@@ -24,8 +29,10 @@ public class MainLambdas {
      */
     public static void main(String[] args) {
 
+	ejecutarEjemploHiloLambda();
 	ejecutarEjemploCalculadora();
 	ejecutarEjemploCadenas();
+
     }
 
     /**
@@ -81,6 +88,37 @@ public class MainLambdas {
 	restarNumeros.operar(numero1, numero2);
 	multiplicarNumeros.operar(numero1, numero2);
 	dividirNumeros.operar(numero1, numero2);
+    }
+
+    /**
+     * Ejecuta un hilo creado a partir de un lambda que solo imprime trazas de log
+     * indicando los pasos que hace se pone el primero en el main para que mientras
+     * esta ejecutando este se hagan las otras operaciones del main por lo tanto
+     * este metodo dejara traza en el principio y el final del metodo ya que la
+     * ejecucion de los demas metodos que contiene el main no excede el tiempo de
+     * espera usado en este caso @see es.maquina.lambdas.interfaces.MainLambda#main
+     */
+    private static void ejecutarEjemploHiloLambda() {
+
+	/**
+	 * Creacion de un objeto Runnable (Generico de los hilos) en el que definimos la
+	 * funcionalidad Impresion de trazas al iniciar el hilo y al acabar una espera
+	 * de @see es.maquina.lambdas.interfaces.MainLambdas#TIEMPO_ESPERA_HILO
+	 */
+	Runnable hilo = (() -> {
+	    try {
+
+		LOGGER.info("Se empieza la ejecucion del hilo: " + Thread.currentThread().getName());
+		Thread.sleep(TIEMPO_ESPERA_HILO);
+		LOGGER.info(Thread.currentThread().getName() + " se desperto despues de " + TIEMPO_ESPERA_HILO / 1000
+			+ " Segundos de hibernacion");
+	    } catch (InterruptedException exception) {
+		LOGGER.warning(exception.getMessage());
+	    }
+	});
+
+	new Thread(hilo, "Hilo Dormilon de 2 Seg").start();
+
     }
 
     /**
